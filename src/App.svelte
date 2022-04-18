8a77d05f3b65dcf866b2afc6f30daf4d52e4e689
ShinDongJun <wq0212@naver.com>
2022-04-19 00:23:54 +0900

    Init svelte and setting router

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import {Route, Link, Router} from "svelte-routing";
+	import Login from './routes/Login.svelte';
+	import Join from './routes/Join.svelte';
+
+	export let url = "";
+</script>
+
+<Router {url}>
+	<nav>
+		<Link to="/">Login</Link>
+		<Link to="join">Join</Link>
+	</nav>
+	<div>
+		<Route path="/"><Login /></Route>
+		<Route path="join"><Join /></Route>
+	</div>
+</Router>
+
+<main>
+</main>
+
+<style>
+</style>
---
+++ src/routes/Login.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<h1>Login</h1>
+
+
+<style>
+</style>
