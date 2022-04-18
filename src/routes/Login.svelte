8ebee90c1a6c469ff1f8c3ad74d91566a73aff4b
ShinDongJun <wq0212@naver.com>
2022-04-19 00:36:40 +0900

    make Login.svelte

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,8 +1,27 @@
 <script>
+    import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
+  
+    const form = useForm();
 </script>
+  
+<form use:form>
+    <h1>Login</h1>
 
-<h1>Login</h1>
+    <input type="email" name="email" use:validators={[required, email]} placeholder="Email"/>
+    <HintGroup for="email">
+        <Hint on="required">이메일은 필수 항목입니다.</Hint>
+        <Hint on="email" hideWhenRequired>유효하지 않은 이메일입니다.</Hint>
+    </HintGroup>
 
+    <input type="password" name="password" use:validators={[required]} placeholder="Password"/>
+    <Hint for="password" on="required">비밀번호는 필수 항목입니다.</Hint>
+
+    <button disabled={!$form.valid}>로그인</button>
+</form>
 
 <style>
+	:global(.touched:invalid) {
+		border-color: red;
+		outline-color: red;
+	}
 </style>
